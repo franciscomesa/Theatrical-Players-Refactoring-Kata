d1d4990abbc548d047af72157a01b36039e4b4f9
Francisco Mesa <franciscomesa@gmail.com>
2025-04-01 16:03:33 +0200

    ♻️ extract buildCurrencyFormat

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -19,11 +19,7 @@ function statement(invoice: Invoice, plays: Plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+  const format = buildCurrencyFormatter();
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -61,3 +57,11 @@ function statement(invoice: Invoice, plays: Plays) {
 }
 
 export { statement };
+
+  function buildCurrencyFormatter() {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format;
+  }
